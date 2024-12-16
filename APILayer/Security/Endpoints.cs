10055fe9e8bf41123688f75550a2eef2356ec570
Phuc Hau <haunhpr024@gmail.com>
2024-12-16 22:28:20 +0700

    Config endpoint for roles

--- APILayer/Security/Endpoints.cs
+++ APILayer/Security/Endpoints.cs
@@ -4,28 +4,46 @@
     {
         public static readonly string[] PublicEndpoints =
         {
+            // AuthController
             "/api/auth/login",
             "/api/auth/register",
             "/api/auth/confirm-email",
-            "/api/img",
             "/api/auth/refresh-token",
             "/api/auth/signin-google",
             "/api/auth/google-response",
-            "/graphql",
+
+            // UserController
+            "/api/user/forgot-password",
+            "/api/user/verify-code",
+            "/api/user/reset-password",
+
+            // ChatController
             "/api/chat/history",
             "/api/chat/send",
-            "/chathub/negotiate",
+            "/api/chat/get-conversations",
+
+            // SignalR
             "/chathub",
+            "/chathub/negotiate",
             "/notificationhub",
             "/notificationhub/negotiate",
-            "/api/chat/get-conversations",
+
+            // Others
+            "/api/img",
+            "/graphql",
             "/api/paypal",
 
         };
 
         public static readonly string[] AdminEndpoints =
         {
-            "/api/user"
+            // UserController
+            "/api/user/get-users",
+            "/api/user/get-user-by-id",
+            "/api/user/delete-user",
+
+            // ChatController
+            "/api/chat/sendAll",
         };
 
         public static readonly string[] CustomerEndpoints =
