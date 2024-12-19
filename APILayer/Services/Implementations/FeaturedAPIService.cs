5aea3364bf049a751cbffa240ca6158033e8df61
Phuc Hau <haunhpr024@gmail.com>
2024-12-19 17:38:42 +0700

    Add Service Featured API

---
+++ APILayer/Services/Implementations/FeaturedAPIService.cs
@@ -0,0 +1,86 @@
+﻿using APILayer.Data;
+using APILayer.Models.Entities;
+using APILayer.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace APILayer.Services.Implementations
+{
+    public class FeaturedAPIService : IFeaturedAPIService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FeaturedAPIService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<FeaturedAPI>> GetAllFeaturedAPIsAsync()
+        {
+            return await _context.FeaturedAPIs
+                .Include(f => f.Api)
+                .Include(f => f.User)
+                .ToListAsync();
+        }
+
+        public async Task<FeaturedAPI?> GetFeaturedAPIByIdAsync(int id)
+        {
+            return await _context.FeaturedAPIs
+                .Include(f => f.Api)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(f => f.Id == id);
+        }
+
+        public async Task<IEnumerable<FeaturedAPI>> GetFeaturedAPIsByUserIdAsync(int userId)
+        {
+            return await _context.FeaturedAPIs
+                .Include(f => f.Api)
+                .Include(f => f.User)
+                .Where(f => f.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<FeaturedAPI>> GetCurrentFeaturedAPIsAsync()
+        {
+            var currentDate = DateTime.UtcNow;
+            return await _context.FeaturedAPIs
+                .Include(f => f.Api)
+                .Include(f => f.User)
+                .Where(f => f.FeaturedFrom <= currentDate && f.FeaturedTo >= currentDate)
+                .ToListAsync();
+        }
+
+        public async Task<FeaturedAPI> CreateFeaturedAPIAsync(FeaturedAPI featuredAPI)
+        {
+            _context.FeaturedAPIs.Add(featuredAPI);
+            await _context.SaveChangesAsync();
+            return featuredAPI;
+        }
+
+        public async Task<FeaturedAPI?> UpdateFeaturedAPIAsync(int id, FeaturedAPI featuredAPI)
+        {
+            var existingFeaturedAPI = await _context.FeaturedAPIs.FindAsync(id);
+            if (existingFeaturedAPI == null)
+                return null;
+
+            existingFeaturedAPI.ApiId = featuredAPI.ApiId;
+            existingFeaturedAPI.UserId = featuredAPI.UserId;
+            existingFeaturedAPI.ReasonForFeature = featuredAPI.ReasonForFeature;
+            existingFeaturedAPI.FeaturedFrom = featuredAPI.FeaturedFrom;
+            existingFeaturedAPI.FeaturedTo = featuredAPI.FeaturedTo;
+
+            await _context.SaveChangesAsync();
+            return existingFeaturedAPI;
+        }
+
+        public async Task<bool> DeleteFeaturedAPIAsync(int id)
+        {
+            var featuredAPI = await _context.FeaturedAPIs.FindAsync(id);
+            if (featuredAPI == null)
+                return false;
+
+            _context.FeaturedAPIs.Remove(featuredAPI);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ APILayer/Services/Interfaces/IFeaturedAPIService.cs
@@ -0,0 +1,15 @@
+﻿using APILayer.Models.Entities;
+
+namespace APILayer.Services.Interfaces
+{
+    public interface IFeaturedAPIService
+    {
+        Task<IEnumerable<FeaturedAPI>> GetAllFeaturedAPIsAsync();
+        Task<FeaturedAPI?> GetFeaturedAPIByIdAsync(int id);
+        Task<IEnumerable<FeaturedAPI>> GetFeaturedAPIsByUserIdAsync(int userId);
+        Task<IEnumerable<FeaturedAPI>> GetCurrentFeaturedAPIsAsync();
+        Task<FeaturedAPI> CreateFeaturedAPIAsync(FeaturedAPI featuredAPI);
+        Task<FeaturedAPI?> UpdateFeaturedAPIAsync(int id, FeaturedAPI featuredAPI);
+        Task<bool> DeleteFeaturedAPIAsync(int id);
+    }
+}
