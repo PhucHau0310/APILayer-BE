87862014b52b7c8f26524dcf39f9d7f291c10524
Phuc Hau <haunhpr024@gmail.com>
2024-12-29 15:26:11 +0700

    Update endpoint for rolemiddlware

--- APILayer/Security/Endpoints.cs
+++ APILayer/Security/Endpoints.cs
@@ -1,10 +1,12 @@
-﻿namespace APILayer.Security
+﻿using APILayer.Models.Entities;
+
+namespace APILayer.Security
 {
     public static class Endpoints
     {
         public static readonly string[] PublicEndpoints =
         {
-            // AuthController
+            // Auth
             "/api/auth/login",
             "/api/auth/register",
             "/api/auth/confirm-email",
@@ -16,36 +18,38 @@
             "/signin-facebook",
             "/facebook-response",
 
-            // UserController
+            // User
             "/api/user/forgot-password",
             "/api/user/verify-code",
             "/api/user/reset-password",
-            "/api/user/get-user-by-name",
-            "/api/user/update-avatar-user",
-            "/api/user/update-username",
-            "/api/user/change-password",
 
-            // ChatController
-            "/api/chat/history",
-            "/api/chat/send",
-            "/api/chat/get-conversations",
-
-            // FAQ
+             // FAQ
             "/api/faq/get-faqs",
             "/api/faq/get-faq-by-id",
             "/api/faq/get-faq-by-userId",
-            "/api/faq/create-faq",
-            "/api/faq/update-faq",
-            "/api/faq/delete-faq",
 
             // API 
+            "/api/api",
             "/api/api/documentation",
-            "/api/api/documentation/{id}", // Để người dùng có thể xem docs
-            "/api/api/version", // Để người dùng có thể xem versions
-            "/api/api/version/{id}",
+            "/api/api/version",
+            "/api/api/featured",
+            "/api/api/featured/current",
+            "/api/api/featured/{id}",
+            "/api/api/featured/user/{userId}",
 
-             "/api/api",
-             "/api/api/documentation",
+            // Chat
+            "/api/chat/history",
+            "/api/chat/send",
+            "/api/chat/get-conversations/{usename}",
+
+            // Notification
+            "/api/notification/send",
+            "/api/notification/send-many",
+            "/api/notification/{id}/read",
+            "/api/notification/unread/{username}",
+            "/api/notification/{username}",
+            "/api/notification/detail/{id}",
+            "/api/notification/{id}",
 
             // SignalR
             "/chathub",
@@ -64,75 +68,74 @@
             // UserController
             "/api/user/get-users",
             "/api/user/get-user-by-id",
+            "/api/user/get-user-by-name",
             "/api/user/delete-user",
+            "/api/user/update-avatar-user",
+            "/api/user/update-username",
+            "/api/user/change-password",
 
-            // ChatController
-            "/api/chat/sendAll",
+            // FAQ
+            "/api/faq/create-faq",
+            "/api/faq/update-faq",
+            "/api/faq/delete-faq",
 
-             // API 
-            "/api/api",
+            // API 
             "/api/api/{id}",
 
-            // Documentation
-            "/api/api/documentation",
+            // API Docs
             "/api/api/documentation/{id}",
 
-            // Version
-            "/api/api/version",
+            // API Version
             "/api/api/version/{id}",
-
-            // Chat
-            "/api/chat/get-conversations/{username}",
-
-            // Notification
-            "/api/notification/send",
-            "/api/notification/send-many",
-            "/api/notification/{username}",
-            "/api/notification/detail/{id}",
-            "/api/notification/{id}",
         };
 
         public static readonly string[] CustomerEndpoints =
         {
-            // Chat
-            "/api/chat/send",
-            "/api/chat/history",
+            // User
+            "/api/user/get-user-by-name",
+            "/api/user/update-avatar-user",
+            "/api/user/update-username",
+            "/api/user/change-password",
+
+            // FAQ
+            "/api/faq/create-faq",
+            "/api/faq/update-faq",
+
+            // API 
+            "/api/api/{id}",
+
+            // API Docs
+            "/api/api/documentation/{id}",
+
+            // API Version
+            "/api/api/version/{id}",
 
             // Others
             "/api/stripe/create-payment-intent",
             "/api/stripe/webhook",
             "/api/paypal/create-order",
-
-            //Notification
-            "/api/notification/{id}/read",
-            "/api/notification/unread/{username}",
         };
 
         public static readonly string[] ProviderEndpoints =
         {
-            // Chat
-            "/api/chat/send",
-            "/api/chat/history",
+            // User
+            "/api/user/get-user-by-name",
+            "/api/user/update-avatar-user",
+            "/api/user/update-username",
+            "/api/user/change-password",
 
-            "/api/stripe/create-payment-intent",
-            "/api/stripe/webhook",
-            "/api/paypal/create-order",
+            // FAQ
+            "/api/faq/create-faq",
+            "/api/faq/update-faq",
 
-            // API Management endpoints
-            "/api/api",
+            // API 
             "/api/api/{id}",
 
-            // Documentation
-            "/api/api/documentation",
+             // API Docs
             "/api/api/documentation/{id}",
 
-            // Version  
-            "/api/api/version",
+            // API Version
             "/api/api/version/{id}",
-
-             //Notification
-            "/api/notification/{id}/read",
-            "/api/notification/unread/{username}",
         };
     }
 }
